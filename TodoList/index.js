9c9eb6d8a35b903cd55648539eaa0d2d9caaa4e7
villion-wong <wanglei5590@gmail.com>
2016-02-22 23:09:23 -0800

    add angular js todo list

---
+++ TodoList/index.js
@@ -0,0 +1,21 @@
+var app = angular.module('todoList', []);
+app.controller('myController', function ($scope) {
+	$scope.tasks = [];
+	$scope.addCheckBox = function() {
+		
+	};
+});
+
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
