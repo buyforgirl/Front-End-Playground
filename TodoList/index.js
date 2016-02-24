573385943e9a3b7d5986b34744fd23cb7f10355f
villion-wong <wanglei5590@gmail.com>
2016-02-23 17:16:39 -0800

    finish todo list in angular js

--- TodoList/index.js
+++ TodoList/index.js
@@ -1,21 +1,28 @@
 var app = angular.module('todoList', []);
+
 app.controller('myController', function ($scope) {
 	$scope.tasks = [];
-	$scope.addCheckBox = function() {
-		$scope.tasks.push($scope.task);
-	};
-});
-
-app.directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
-
-                event.preventDefault();
-            }
+	$scope.add = function() {
+        if (event.keyCode == 13 && $scope.task !== '') {
+            $scope.tasks.push({text:$scope.task, finish:false});
+            $scope.task = '';
+        } 
+	}
+    $scope.finishAll = function() {
+            $scope.tasks.forEach(function(obj) {
+                obj.finish = true;
+            });
+    }
+    $scope.remain = function() {
+        var remainArray = $scope.tasks.filter(function(obj){
+            return obj.finish == false;
         });
-    };
+        return remainArray.length;
+    }
+    $scope.clearAll = function() {
+        $scope.tasks.forEach(function(obj) {
+            obj.finish = false;
+        });
+    }
 });
+
